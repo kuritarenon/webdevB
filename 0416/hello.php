92bc72e4a50024631b91a4c3c4cad4094bf78f56
kuritarenon <kurita.0864@gmail.com>
2026-04-16 11:44:49 +0900

    Initial commit

---
+++ 0416/hello.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>最初のPHPプログラム</title>
+</head>
+
+<body>
+    <h1>さいしょのPHPプログラム</h1>
+    <b>
+        <?php
+        echo "こんにちは";
+        ?>
+    </b>
+</body>
+
+</html>
---
+++ 0416/num.php
@@ -0,0 +1,10 @@
+<?php
+$a = 10;
+$b = 2;
+echo $a + $b;
+echo $a - $b;
+echo $a * $b;
+echo $a / $b;
+$c = 3;
+echo $a % $c;
+echo $a ** 2;
