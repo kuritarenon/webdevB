c62699f494df7587f309bc9cf3fc172af303fa2e
kuritarenon <kurita.0864@gmail.com>
2026-04-16 12:42:01 +0900

    0416
    ~47pまで

---
+++ 0416/if.php
@@ -0,0 +1,19 @@
+<?php
+$condition = false;
+if ($condition) echo "条件はtrueです。";
+
+$a = 2;
+if ($a === 1) {
+    echo "aは1です。";
+}
+
+$b = 1;
+$c = 2;
+if ($b < $c) {
+    echo "aはbよりも小さいです。";
+}
+
+$d = "1";
+if ($d == 1) {
+    echo "aは1です。";
+}
