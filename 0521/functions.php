312be0826b2c5e79812c41c6520ce96afb8b231c
kuritarenon <kurita.0864@gmail.com>
2026-05-28 12:43:07 +0900

    0528
    formの練習問題

--- 0521/functions.php
+++ 0521/functions.php
@@ -1,4 +1,5 @@
 <?php
+//xss対策の関数
 function str2html(string $string): string
 {
     return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
--- 0521/question10/search.php
+++ 0521/question10/search.php
@@ -6,3 +6,19 @@ if ($fp === false) {
     echo "ファイルのオープンに失敗しました。";
     exit;
 }
+
+//formからの値取得
+$keyword = $_POST["keyword"];
+var_dump($keyword . "<br>");
+
+while ($row = fgetcsv($fp)) {
+    foreach ($row as $column) {
+        if ($keyword === $column) {
+            echo "曲名:" . $row[0] . "<br>";
+            echo "アーティスト名：" . $row[1] . "<br>";
+            echo "ジャンル：" . $row[2] . "<br>";
+            echo "リリース年：" . $row[3] . "<br>";
+            echo "メモ：" . $row[4] . "<br>";
+        }
+    }
+}
